c459100588428975c0c8df9e71d5e38cb1cb21f5
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2020-09-01 12:59:23 +0200

    Added unfinished backup files
    Files-
    SearchFilter.vue
    SearchFilter.spec.js

---
+++ src/components/SearchFilter.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="search-overlay">
+        <!-- <button class="menu"></button>
+        <input type="text" v-model="userInput"/>
+        <label>
+            <input type="range" min="1" max="3" v-model="size">
+        </label>
+        <label>
+            <input type="range" min="24" max="45">
+        </label>
+        <label>
+            <input type="range" min="1" max="3">
+        </label>
+        <label>
+            <input type="range" min="1" max="5">
+        </label>
+        <button class="search"></button> -->
+
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            userInput: '',
+            size: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+.search-overlay {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    margin: 0px;
+    top: 0px;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(8, 1fr);
+}
+</style>
---
+++ tests/unit/searchFilter.spec.js
@@ -0,0 +1,36 @@
+import { shallowMount } from "@vue/test-utils";
+import SearchFilter from "@/components/SearchFilter.vue";
+import { iterator } from "core-js/fn/symbol";
+
+describe("SearchFilter.vue", async () => {
+    const wrapper
+    beforeEach(() => {
+        wrapper = shallowMount(SearchFilter);
+    })
+})
+
+/* 
+Som användare vill jag kunna filtrera listan. 
+Som användare vill jag ha enkel tillgång till sökfunktionen. 
+Som användare vill jag att filtret ska ge mig möjlighet att välja underlag.
+Som användare vill jag att filtret ska ge mig möjlighet att välja märke.
+Som användare vill jag att filtret ska ge mig möjlighet att välja storleksklass.
+*/
+
+it("should show the menu/exit button when loaded", () => {
+
+})
+
+it("should show the inputfield when loaded", () => {
+
+})
+
+it("should show the filters when loaded", () => {
+
+})
+
+it("should show the search button when loaded", () => {
+
+})
+
+
