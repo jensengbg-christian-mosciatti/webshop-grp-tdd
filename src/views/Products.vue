9f3e5934dbcb5859e85fb9932b76fcbab32a1c79
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-09-03 13:00:16 +0200

    changes to make store state to work

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,45 +1,50 @@
 <template>
   <div class="home">
-    <ul>
-      <!--<li class="productList" v-for="item of productList" :key="item.id"> <img class="images" :src="item.img" alt="images"> {{ item.brand }} {{ item.type }}, {{ item.price }} </li>-->
-      <ProductList />
-    </ul>
+    <!-- <ul> -->
+    <!--<li class="productList" v-for="item of productList" :key="item.id"> <img class="images" :src="item.img" alt="images"> {{ item.brand }} {{ item.type }}, {{ item.price }} </li>-->
+    <!-- <ProductList :productDataList="productList" /> -->
+    <ProductList />
+    <!-- </ul> -->
   </div>
 </template>
 
 <script>
-import ProductList from '../components/ProductList';
+import ProductList from '../components/ProductList'
 
 export default {
   name: 'Products',
   data() {
     return {
-      //productList: this.$store.state.productList
-
+      productList: this.$store.state.productList,
     }
   },
+  computed: {
+    // productList() {
+    //   return this.$store.state.productList
+    // },
+  },
   components: {
-    ProductList
-  }
+    ProductList,
+  },
 }
 </script>
 
 <style scoped>
-  .productList {
-    background-color: white;
-    color: black;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    margin: 2em;
-  }
+.productList {
+  background-color: white;
+  color: black;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 2em;
+}
 
-  .images {
-    height: 100px;
-    width: 100px;
-    display: flex;
-    margin: 5em;
-    margin-bottom: 1em;
-    margin-top: 1em;
-  }
+.images {
+  height: 100px;
+  width: 100px;
+  display: flex;
+  margin: 5em;
+  margin-bottom: 1em;
+  margin-top: 1em;
+}
 </style>
--- tests/unit/ProductList.spec.js
+++ tests/unit/ProductList.spec.js
@@ -1,50 +1,50 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import Products from '@/components/ProductList.vue'
-//import ProductList from '@/assets/db/products/products.json'
+import productList from '@/assets/db/products/products.json'
 
 describe('ProductList.vue', () => {
-    let wrapper;
-    beforeEach(() => {
-        wrapper = shallowMount(Products);
-    })
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallowMount(Products)
+  })
 
-    it('should display the brand of the product when rendered', () => {
-      const wrapper = mount(Products)
+  it('should display the brand of the product when rendered', () => {
+    const wrapper = mount(Products)
 
-      expect(wrapper.exists()).toBe(true)
-      expect(wrapper.find('.brand').exists()).toBe(false)
-      expect(wrapper.findAll('div').exists()).toBe(true)
-      expect(wrapper.findAll('.brand').exists()).toBe(false)
-    })
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.find('.brand').exists()).toBe(false)
+    expect(wrapper.findAll('div').exists()).toBe(true)
+    expect(wrapper.findAll('.brand').exists()).toBe(false)
+  })
 
-    it('should display the size of the product when rendered', () => {
-      const wrapper = mount(Products)
+  it('should display the size of the product when rendered', () => {
+    const wrapper = mount(Products)
 
-      expect(wrapper.exists()).toBe(true)
-      expect(wrapper.find('.size').exists()).toBe(false)
-      expect(wrapper.findAll('div').exists()).toBe(true)
-      expect(wrapper.findAll('.size').exists()).toBe(false)
-    })
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.find('.size').exists()).toBe(false)
+    expect(wrapper.findAll('div').exists()).toBe(true)
+    expect(wrapper.findAll('.size').exists()).toBe(false)
+  })
 
-    it('should display the type of the product when rendered', () => {
-      const wrapper = mount(Products)
+  it('should display the type of the product when rendered', () => {
+    const wrapper = mount(Products)
 
-      expect(wrapper.exists()).toBe(true)
-      expect(wrapper.find('.type').exists()).toBe(false)
-      expect(wrapper.findAll('div').exists()).toBe(true)
-      expect(wrapper.findAll('.type').exists()).toBe(false)
-    })
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.find('.type').exists()).toBe(false)
+    expect(wrapper.findAll('div').exists()).toBe(true)
+    expect(wrapper.findAll('.type').exists()).toBe(false)
+  })
 
-    it('should display the price of the product when rendered', () => {
-      const wrapper = mount(Products)
+  it('should display the price of the product when rendered', () => {
+    const wrapper = mount(Products)
 
-      expect(wrapper.exists()).toBe(true)
-      expect(wrapper.find('.price').exists()).toBe(false)
-      expect(wrapper.findAll('div').exists()).toBe(true)
-      expect(wrapper.findAll('.price').exists()).toBe(false)
-    })
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.find('.price').exists()).toBe(false)
+    expect(wrapper.findAll('div').exists()).toBe(true)
+    expect(wrapper.findAll('.price').exists()).toBe(false)
+  })
 
-    /*it('should display the image of the product when rendered', () => {
+  /*it('should display the image of the product when rendered', () => {
       const wrapper = mount(Products)
       expect(wrapper.exists()).toBe(true)
       expect(wrapper.find('.product-icon').exists()).toBe(false)
@@ -52,39 +52,37 @@ describe('ProductList.vue', () => {
       expect(wrapper.findAll('.product-icon').exists()).toBe(false)
     })*/
 
-    it('should display details of the product when clicked on', async () => {
-      const wrapper = mount(Products)
+  it('should display details of the product when clicked on', async () => {
+    const wrapper = mount(Products)
 
-      const detailButton = wrapper.find('.detail-button')
+    const detailButton = wrapper.find('.detail-button')
 
-      await detailButton.trigger('click');
+    await detailButton.trigger('click')
+  })
+
+  it('should display the brand of the product when rendered', () => {
+    // const wrapper = mount(Products)
+    // let ProductList
+    const wrapper = mount(Products, {
+      mocks: {
+        $store: {
+          state: { productList: productList },
+        },
+      },
     })
 
+    // console.log(productList)
+    const firstElement = wrapper.findAll('.product-data').at(0)
 
+    expect(wrapper.findAll('.product-data').text()).toBe('ProductList')
 
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.find(productList.brand).exists()).toBe(false)
+    expect(wrapper.findAll('li').exists()).toBe(true)
+    expect(wrapper.findAll(item.brand).exists()).toBe(false)
+  })
 
-
-
-    /*it('should display the brand of the product when rendered', () => {
-        const wrapper = mount(Products);
-        let ProductList;
-        const wrapper = mount(Products, {
-            mocks: {
-              $store: {
-                state: { productList: ProductList }
-              }
-            }
-        })
-
-        expect(wrapper.find(".productList").text()).toBe("ProductList")
-
-        expect(wrapper.exists()).toBe(true)
-        expect(wrapper.find(item.brand).exists()).toBe(false)
-        expect(wrapper.findAll('li').exists()).toBe(true)
-        expect(wrapper.findAll(item.brand).exists()).toBe(false)
-    })*/
-
-    /*it('should display the image of the product when rendered', () => {
+  /*it('should display the image of the product when rendered', () => {
 
     })
 
