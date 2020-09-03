cc5d6a16cc78cc705024f7d9ff9c19d83867f293
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-09-03 09:47:40 +0200

    added shoppingcart view and test

---
+++ src/components/Product.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button @click="addToCart">Add To Cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cart: [],
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('cart')) {
+      try {
+        this.cart = JSON.parse(localStorage.getItem('cart'))
+      } catch (e) {
+        localStorage.removeItem('cart')
+      }
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart.push({ item: 'Banana', price: 123, description: 'Bla bla bla...' })
+      this.saveToLocalStorage()
+    },
+    saveToLocalStorage() {
+      const parsed = JSON.stringify(this.cart)
+      localStorage.setItem('cart', parsed)
+    },
+  },
+}
+</script>
+
+<style></style>
