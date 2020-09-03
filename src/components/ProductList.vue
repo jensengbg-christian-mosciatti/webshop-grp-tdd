8cad0b3c943179260e3967d25bdb87531c5aa3fc
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-09-03 14:36:00 +0200

    made changes to make store to work

---
+++ src/components/ProductList.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <h1>Products</h1>
+    <!-- <button v-on:click="getProductData">Show Products</button> -->
+    <div v-for="productData in productDataList" :key="productData.id" class="product-data">
+      <div class="product-stats detail-button">
+        <div class="product-icon">
+          <img :src="productData.img" alt="products" />
+        </div>
+        <div class="brand size">
+          <span>{{ productData.brand }} | {{ productData.size }}</span>
+        </div>
+        <div class="type">
+          <span>{{ productData.type }}</span>
+        </div>
+        <div class="price">
+          <span>{{ productData.price }} kr</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductsList',
+  data() {
+    return {
+      // productDataList: [],
+    }
+  },
+  props: {
+    productDataList: Array,
+  },
+  methods: {
+    // getProductData() {
+    //   fetch('products.json')
+    //     .then(response => response.json())
+    //     .then(data => (this.productDataList = data))
+    // },
+  },
+}
+</script>
+
+<style scoped>
+.product-data {
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+  margin-left: 20px;
+  border-bottom: 2px solid #ccc;
+  padding: 20px;
+}
+
+.product-icon {
+  flex-grow: 1;
+}
+
+.product-stats {
+  flex-grow: 8;
+  text-align: left;
+  padding-left: 20px;
+}
+
+.product-stats .brand .size .type .price {
+  font-size: 30px;
+}
+
+img {
+  width: 70px;
+}
+
+button {
+  padding: 10px;
+  background-color: #1aa832;
+  color: white;
+  border: 1px solid #ccc;
+}
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,60 +1,63 @@
 <template>
   <div class="home">
-
-    <SearchFilter v-on:showClose="toggleSearch" v-on:searchAndClose="searched" v-if="showSearch"/>
-    <img alt="Vue logo" src="../assets/logo.png" />
-
-    <ul>
-      <!--<li class="productList" v-for="item of productList" :key="item.id"> <img class="images" :src="item.img" alt="images"> {{ item.brand }} {{ item.type }}, {{ item.price }} </li>-->
-      <ProductList />
-    </ul>
+    <SearchFilter v-on:showClose="toggleSearch" v-on:searchAndClose="searched" v-if="showSearch" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <button @click="toggleSearch">Search</button>
+    <!-- <ul> -->
+    <!--<li class="productList" v-for="item of productList" :key="item.id"> <img class="images" :src="item.img" alt="images"> {{ item.brand }} {{ item.type }}, {{ item.price }} </li>-->
+    <ProductList :productDataList="filteredList" />
+    <!-- </ul> -->
   </div>
 </template>
 
 <script>
 import SearchFilter from '../components/SearchFilter'
-import ProductList from '../components/ProductList';
+import ProductList from '../components/ProductList'
 
 export default {
   name: 'Products',
   data() {
     return {
       productList: this.$store.state.productList,
-      showSearch: true
+      showSearch: false,
+      filterSize: 'kids',
     }
   },
-  components: { SearchFilter },
+  computed: {
+    filteredList() {
+      return this.productList
+    },
+  },
+  components: { SearchFilter, ProductList },
   methods: {
     toggleSearch() {
       this.showSearch = !this.showSearch
     },
     searched() {
       this.showSearch = false
-    }
+    },
 
-      //productList: this.$store.state.productList
-
-    }
-  }
+    //productList: this.$store.state.productList
+  },
 }
 </script>
 
 <style scoped>
-  .productList {
-    background-color: white;
-    color: black;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    margin: 2em;
-  }
+.productList {
+  background-color: white;
+  color: black;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 2em;
+}
 
-  .images {
-    height: 100px;
-    width: 100px;
-    display: flex;
-    margin: 5em;
-    margin-bottom: 1em;
-    margin-top: 1em;
-  }
+.images {
+  height: 100px;
+  width: 100px;
+  display: flex;
+  margin: 5em;
+  margin-bottom: 1em;
+  margin-top: 1em;
+}
 </style>
