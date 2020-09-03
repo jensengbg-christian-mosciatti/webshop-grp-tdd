333f5b0d0174e09252faaf8788aba4c74095c486
johanna-ohrnberg <j-ohrnberg@live.se>
2020-09-03 10:03:42 +0200

    update products and productList

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <ul>
-      <li v-for="item of productList" :key="item.id">{{ item.brand }} {{ item.type }}, {{ item.price }}</li>
+      <!--<li class="productList" v-for="item of productList" :key="item.id"> <img class="images" :src="item.img" alt="images"> {{ item.brand }} {{ item.type }}, {{ item.price }} </li>-->
+      <ProductList />
     </ul>
   </div>
 </template>
 
 <script>
+import ProductList from '../components/productList';
+
 export default {
   name: 'Products',
   data() {
     return {
-      productList: this.$store.state.productList,
+      //productList: this.$store.state.productList
+
     }
   },
-
-  components: {},
+  components: {
+    ProductList
+  }
 }
 </script>
+
+<style scoped>
+  .productList {
+    background-color: white;
+    color: black;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin: 2em;
+  }
+
+  .images {
+    height: 100px;
+    width: 100px;
+    display: flex;
+    margin: 5em;
+    margin-bottom: 1em;
+    margin-top: 1em;
+  }
+</style>
