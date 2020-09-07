68b5f2533fa1700f5480f34ab3fae376435454a1
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-09-07 17:53:02 +0200

    added productDetails.spec.js

---
+++ tests/unit/productDetails.spec.js
@@ -0,0 +1,62 @@
+import { mount, shallowMount } from '@vue/test-utils'
+import ProductDet from '@/views/ProductDetails'
+import productList from '@/assets/db/products/products.json'
+
+describe('Testing Product Details', () => {
+  it('should return a "Product not found" text when not returning any product', () => {
+    const notExistingProductId = -2
+    const wrapper = mount(ProductDet, {
+      mocks: {
+        $store: {
+          state: { productList: productList },
+        },
+      },
+      propsData: {
+        id: notExistingProductId,
+      },
+    })
+
+    const noProductsFound =
+      wrapper.find('.fallback-section').element.style.display !== 'none'
+
+    expect(noProductsFound).toBe(true)
+  })
+
+  it('should not return a "Product not found" text when returning a product', () => {
+    const existingProductId = 1
+    const wrapper = mount(ProductDet, {
+      mocks: {
+        $store: {
+          state: { productList: productList },
+        },
+      },
+      propsData: {
+        id: existingProductId,
+      },
+    })
+
+    const noProductsFound =
+      wrapper.find('.fallback-section').element.style.display === 'none'
+
+    expect(noProductsFound).toBe(true)
+  })
+
+  it('should not return a "Product not found" text when returning a product', () => {
+    const existingProductId = 1
+    const wrapper = mount(ProductDet, {
+      mocks: {
+        $store: {
+          state: { productList: productList },
+        },
+      },
+      propsData: {
+        id: existingProductId,
+      },
+    })
+
+    const noProductsFound =
+      wrapper.find('.fallback-section').element.style.display === 'none'
+
+    expect(noProductsFound).toBe(true)
+  })
+})
