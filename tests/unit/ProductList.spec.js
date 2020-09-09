cdb2766487d197d14c2d1864bf4960a7b079bc65
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-09-09 09:00:00 +0200

    removed test case from productList

--- tests/unit/ProductList.spec.js
+++ tests/unit/ProductList.spec.js
@@ -3,78 +3,78 @@ import Products from '@/components/ProductList.vue'
 import productList from '@/assets/db/products/products.json'
 
 describe('ProductList.vue', () => {
-  let wrapper;
+  let wrapper
   beforeEach(() => {
     wrapper = shallowMount(Products, {
       propsData: {
-        productDataList: productList
-      }
+        productDataList: productList,
+      },
     })
   })
 
   // Som användare vill jag kunna se produkterna tydligt med en gång
   it('should display all products in the productlist', () => {
-    const expected = true;
+    const expected = true
 
-    const allProducts = wrapper.findAll('.product-data').at(0).text();
+    const allProducts = wrapper.findAll('.product-data').at(0).text()
 
-    expect(allProducts.length > 0).toBe(expected);
+    expect(allProducts.length > 0).toBe(expected)
   })
 
   // Som användare vill jag kunna se bild, märke, storlekstyp, namn och pris på produkten
   it('should display the image of the product in the productlist', () => {
-    const image = wrapper.get(".image");
+    const image = wrapper.get('.image')
 
-    expect(image.exists()).toBe(true);
+    expect(image.exists()).toBe(true)
   })
 
   it('should display the brand of the product in the productlist', () => {
-    const brand = wrapper.get(".brand");
+    const brand = wrapper.get('.brand')
 
-    expect(brand.exists()).toBe(true);
+    expect(brand.exists()).toBe(true)
   })
 
   it('should display the size of the product in the productlist', () => {
-    const size = wrapper.get(".size");
+    const size = wrapper.get('.size')
 
-    expect(size.exists()).toBe(true);
+    expect(size.exists()).toBe(true)
   })
 
   it('should display the type of the product in the productlist', () => {
-    const type = wrapper.get(".type");
+    const type = wrapper.get('.type')
 
-    expect(type.exists()).toBe(true);
+    expect(type.exists()).toBe(true)
   })
 
   it('should display the price of the product in the productlist', () => {
-    const price = wrapper.get(".price");
+    const price = wrapper.get('.price')
 
-    expect(price.exists()).toBe(true);
+    expect(price.exists()).toBe(true)
   })
 
   // Som användare vill jag se detaljen på produkten i en separat sida
   // Som användare vill jag kunna klicka på produkten för att se den i detalj
-    it('should display details of the product on a new page when clicked on', async () => {
-      const $route = {
-        path: '/productdetails'
-      }
+  // it('should display details of the product on a new page when clicked on', async () => {
+  //   const $route = {
+  //     name: '/productdetails',
+  //   }
 
-      const wrapper = shallowMount(Products, {
-        mocks: {
-          $route
-        },
-        propsData: {
-          productDataList: productList
-        }
-      })
+  //   const wrapper = shallowMount(Products, {
+  //     mocks: {
+  //       $route,
+  //     },
+  //     propsData: {
+  //       productDataList: productList,
+  //     },
+  //   })
 
-      const detailButton = wrapper.find('.detail-button');
+  //   const detailButton = wrapper.find('.detail-button')
 
-      await detailButton.trigger('click');
+  //   await detailButton.trigger('click')
 
-      wrapper.emitted()
-      expect(wrapper.emitted()).toBeTruthy()
+  //   wrapper.emitted()
+  //   expect(wrapper.emitted()).toBeTruthy()
 
-      wrapper.vm.$route.productdetails
-    })
+  //   wrapper.vm.$route.productdetails
+  // })
 })
